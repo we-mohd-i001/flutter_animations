29c319bf3f007ad8a1dfe9967a9c5a3c180e384b
Mohd Imran <mohd-i001@webreinvent.com>
2024-03-05 12:46:32 +0530

    git fix

--- lib/models/trip.dart
+++
@@ -1,8 +0,0 @@
-class Trip {
-  final String title;
-  final String price;
-  final String nights;
-  final String img;
-
-  Trip({required this.title, required this.price, required this.nights, required this.img});
-}
