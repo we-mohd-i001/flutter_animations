0b8d1a63764775b89910d2c2abd20b63198e7ac5
Mohd Imran <mohd-i001@webreinvent.com>
2024-03-05 14:30:39 +0530

    Added like button animation

--- lib/shared/heart.dart
+++ lib/shared/heart.dart
@@ -6,32 +6,58 @@ class Heart extends StatefulWidget {
 }
 
 class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
+  bool isFavorite = false;
   late AnimationController _controller;
+  late Animation _colorAnimation;
+
   @override
   void initState() {
     super.initState();
-
     _controller = AnimationController(
       vsync: this,
-      duration: Duration(milliseconds: 200),
+      duration: Duration(milliseconds: 300),
     );
 
-    _controller.forward();
+    _colorAnimation = ColorTween(
+      begin: Colors.grey,
+      end: Colors.red,
+    ).animate(_controller);
+
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          isFavorite = true;
+        });
+      }
+      if (status == AnimationStatus.dismissed) {
+        setState(() {
+          isFavorite = false;
+        });
+      }
+    });
 
     _controller.addListener(() {
-      print(_controller.value);
+      //print(_controller.value);
+      print(_colorAnimation.value);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        Icons.favorite,
-        color: Colors.grey[400],
-        size: 30,
-      ),
-      onPressed: () {},
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (BuildContext context, Widget? child) {
+        return IconButton(
+          icon: Icon(
+            Icons.favorite,
+            color: _colorAnimation.value,
+            size: 30,
+          ),
+          onPressed: () {
+            isFavorite ? _controller.reverse() : _controller.forward();
+          },
+        );
+      },
     );
   }
 }
